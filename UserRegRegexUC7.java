c9b9c0b85d6bf63dc904e1e9311ae349e285cfab
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-09-26 17:47:53 +0530

    Add UC7

--- UserRegRegexUC7.java
+++ UserRegRegexUC7.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegRegexUC6 {
+public class UserRegRegexUC7 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the First Name");
@@ -52,11 +52,9 @@ public class UserRegRegexUC6 {
 			System.out.println("Invalid Mobile Number");
 	
 	System.out.println("Enter the Password");
-	String password = sc.nextLine();
-	String pattern_password = "^(?=.*[A-Z])[\\s]{8,}";
-	Pattern p6 = Pattern.compile(pattern_password);
-	Matcher m6 = p6.matcher(password);
-	if(m6.matches())
+	String password = sc.next();
+	String pattern_password = "(?=.*[0-9])(?=.*[A-Z])[\\s]{8,}";
+	if(Pattern.matches(pattern_password,password))
 		 System.out.println("Valid Passwordr");
 	
 	else
